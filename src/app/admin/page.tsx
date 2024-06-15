b234572b8592d3ec74c5128198cc32e47c001917
Barzeizm <rifki2641@gmail.com>
2024-06-15 23:25:28 +0700

    admin title

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { auth } from "@/lib/auth";
 import {SignOut} from "@/components/signout-button";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+}
 export async function page() {
   const session = await auth();
   return (
